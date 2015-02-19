a352e64374fa7dc1cf7d2fa539219bd14d618850
Reyes Ruiz <reyes@digitalruiz.com>
2015-02-19 01:06:44 -0800

    adding variables to track number of guesses that user has

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -8,4 +8,9 @@ print "What's your name? "
 input = gets
 name = input.chomp
 puts "Welcome, #{name}!"
-
+puts "I have a number between 1 and 100."
+puts "Guess it"
+target = rand(100)+1
+num_guesses = 0
+remaining_guesses = 10 - num_guesses
+puts remaining_guesses.to_s + " guesses left."
