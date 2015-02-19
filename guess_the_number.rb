dd605bd5ec01d326a22b21c03bbb4969451a1e18
Reyes Ruiz <reyes@digitalruiz.com>
2015-02-19 00:46:13 -0800

    Initial commit of gues the number practice script

---
+++ guess_the_number.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+#Guess the number game:
+#Reyes Ruiz
+#
+
+puts "Welcome to 'Guess My Number!'"
+print "What's your name? "
+input = gets
+name = input.chomp
+puts "Welcome, #{name}!"
+
