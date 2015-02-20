31b5055a3f80e0af73a8fa5888cd1610cddaa7ae
Reyes Ruiz <reyes@digitalruiz.com>
2015-02-19 23:07:46 -0800

    adding if statment

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -14,3 +14,19 @@ target = rand(100)+1
 num_guesses = 0
 remaining_guesses = 10 - num_guesses
 puts remaining_guesses.to_s + " guesses left."
+print "Make a guess: "
+guess = gets.to_i
+
+if guess < target
+	puts "oops. Your guess was LOW."
+elsif guess > target
+	puts "oops. Your guess was HIGH."
+elsif guess == target
+	puts "good job, #{name}!"
+	puts "you guessed my number in #{num_guesses} guesses!"
+	guessed_it = true
+end
+
+if not guessed_it
+	puts "Sorry. you didnt get my number. (It was #{target}.)"
+end
