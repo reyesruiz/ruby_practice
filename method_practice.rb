328868552d6de146af7658e4e1600223d47401a8
Reyes Ruiz <reyes@digitalruiz.com>
2015-02-22 23:19:47 -0800

    adding method_practice.rb

---
+++ method_practice.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+#Methods practice:
+#Reyes Ruiz
+#
+
+def accelerate
+	puts "Stepping on the gas"
+	puts "Speeding up"
+end
+
+def sound_horn
+	puts "Pressing the horn button"
+	puts "Beep beep!"
+end
+
+def use_headlights(brightness)
+	puts "Turning on #{brightness} headlights"
+	puts "Watch out for deer!"
+end
+
+sound_horn
+accelerate
+use_headlights("hi-Beam")
+
