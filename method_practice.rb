59a1eed690b6ea5fbffb8bb8034f2e3f6a031bb5
Reyes Ruiz <reyes@digitalruiz.com>
2015-02-22 23:41:13 -0800

    adding methods with return values

--- method_practice.rb
+++ method_practice.rb
@@ -18,8 +18,17 @@ def use_headlights(brightness = "low-beam")
 	puts "Watch out for deer!"
 end
 
+def mileage(miles_driven, gas_used)
+	return miles_driven / gas_used
+end
+
+
+
 sound_horn
 accelerate
 use_headlights
 use_headlights("hi-Beam")
-
+trip_mileage = mileage(400, 12)
+puts "You got #{trip_mileage} MPG on this trip"
+lifetime_mileage = mileage(11432, 366)
+puts "This car averages #{lifetime_mileage} MPG."
