08aab203a371bc23bf341e67b6474ac7118a9412
Reyes Ruiz <reyes@digitalruiz.com>
2015-02-23 17:03:44 -0800

    adding class_practice.rb

---
+++ class_practice.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#Class practice:
+#Reyes Ruiz
+#
+
+class Dog
+	def talk
+		puts "Bark!"
+	end
+	def move(destination)
+		puts "Running to the #{destination}."
+	end
+end
+
+fido = Dog.new
+rex = Dog.new
+
+fido.talk
+rex.move("food bowl")
+
