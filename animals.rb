30383b0e3c092a541dd6513f495e6dc507ea6c68
Reyes Ruiz <reyes@digitalruiz.com>
2016-08-25 23:57:44 -0700

    Making it more efficient by using attr_accessor

--- animals.rb
+++ animals.rb
@@ -4,18 +4,7 @@
 #
 
 class Dog
-	def name=(new_value)
-		@name = new_value
-	end
-	def name
-		@name
-	end
-	def age=(new_value)
-		@age = new_value
-	end
-	def age
-		@age
-	end
+	attr_accessor :name, :age
 	def report_age
 		puts "#{@name} is #{@age} years old."
 	end
